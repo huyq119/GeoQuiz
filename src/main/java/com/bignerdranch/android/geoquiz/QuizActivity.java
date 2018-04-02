8a16f58f058a6ef46892f3be7d3655658aa77499
huyuqi <yuqi.hU@gmail.com>
2018-04-02 10:42:58 +0800

    update

--- src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,32 +2,120 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private boolean isoncl=true;
 
+    private Question[] mQuestionBank = new Question[]{
+        new Question(R.string.question_australia,true),
+        new Question(R.string.question_oceans,true),
+        new Question(R.string.question_mideast,false),
+        new Question(R.string.question_africa,false),
+        new Question(R.string.question_americas,true),
+        new Question(R.string.question_asin,true)
+    };
+    private int mCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+//        检查存储的build信息
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
         mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                if(isoncl){
+                    checkAnswer(true);
+                    isoncl=false;
+                }
             }
         });
         mFalseButton = findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                if(isoncl){
+                    checkAnswer(false);
+                    isoncl=false;
+                }
             }
         });
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+                isoncl = true;
+            }
+        });
+        updateQuestion();
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+//    通过onSaveInstanceState保存数据,使屏幕翻转时保存mCurrentIndex的值
+    @Override
+    public void onSaveIn stanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG,"onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResid = 0;
+        if (userPressedTrue == answerIsTrue){
+            messageResid = R.string.correct_toast;
+            } else {
+            messageResid = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResid, LENGTH_SHORT).show();
     }
 }
